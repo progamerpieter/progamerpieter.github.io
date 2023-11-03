9b299623982dd1b20dc1636e9e1edd8aa692ea74
progamerpieter <pieterheidt4@gmail.com>
2023-11-03 10:23:31 +0100

    finish theme.js and update sitemap

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,33 +1,39 @@
-function checkColorMode() {
-  if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  ) {
-    return "Dark";
-  } else if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: light)").matches
-  ) {
-    return "light";
-  } else {
-    return "unknown";
+if (sessionStorage.getItem("Theme") === "Dark") {
+  Darkmode();
+} else if (sessionStorage.getItem("Theme") === "Light") {
+  Lightmode();
+} else {
+  function checkColorMode() {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "Dark";
+    } else if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: light)").matches
+    ) {
+      return "light";
+    } else {
+      return "unknown";
+    }
   }
 }
-
 // Example usage:
 let colorMode = checkColorMode();
-console.log(`The user's color mode is ${colorMode}`);
 
 if (colorMode == "Dark") {
-    Darkmode();
+  Darkmode();
 } else if (colorMode == "Light") {
-    Lightmode();
+  Lightmode();
 }
 
 function Darkmode() {
   document.querySelector("#body").setAttribute("data-bs-theme", "dark");
+  sessionStorage.setItem("Theme", "Dark");
 }
 
 function Lightmode() {
   document.querySelector("#body").setAttribute("data-bs-theme", "light");
+  sessionStorage.setItem("Theme", "Light");
 }
