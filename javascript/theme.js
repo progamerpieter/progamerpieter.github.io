77b494a070c2562a4c42d7aa33b2af60a9d6ceda
progamerpieter <pieterheidt4@gmail.com>
2024-01-20 22:09:10 +0100

    fix theme bug and add back arrow on about-me.html

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,32 +1,40 @@
+//auto check what theme must be used
 if (sessionStorage.getItem("Theme") === "Dark") {
   Darkmode();
+  console.log("session dark");
 } else if (sessionStorage.getItem("Theme") === "Light") {
   Lightmode();
+  console.log("session light");
 } else {
-  function checkColorMode() {
+   //check default OS theme
     if (
       window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
+      console.log("dark")
       Darkmode();
     } else if (
       window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: light)").matches
     ) {
+      console.log("light");
       Lightmode();
     } else {
-      Lightmode();
+      
+      console.log("nothing");
     }
-  }
+  
 }
 
-
+//theme triggers
 function Darkmode() {
   document.querySelector("#body").setAttribute("data-bs-theme", "dark");
   sessionStorage.setItem("Theme", "Dark");
+  console.log("darkmode trigger");
 }
 
 function Lightmode() {
   document.querySelector("#body").setAttribute("data-bs-theme", "light");
   sessionStorage.setItem("Theme", "Light");
+  console.log("lightmode trigger");
 }
