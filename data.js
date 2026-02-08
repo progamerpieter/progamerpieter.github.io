4d937bbb7abbe62c4ed0a9fa5734a2164bbf45a2
progamerpieter <99116027+progamerpieter@users.noreply.github.com>
2026-02-08 23:00:41 +0100

    Grote vanalles update

---
+++ data.js
@@ -0,0 +1,12 @@
+const projecten = {
+  "padellen-videoclip": {
+    titel: "Turfy gang - Padellen (Muziek Video)",
+    beschrijving: "Voor een opdracht voor mijn studie AV Specialist heb ik deze videoclip gemaakt. Ik heb samengewerkt met 4 medestudenten. Bij dit project heb ik actief bijgedragen aan álle fases, van preproductie tot oplevering.",
+    videoUrl: "https://www.youtube.com/embed/lGq78Ilappw?si=a0xE5OLh-xMcNqBe"
+  },
+  "video-2": {
+    titel: "Bedrijfsvideo Bedrijf X",
+    beschrijving: "Een professionele edit voor een lokale klant.",
+    videoUrl: "https://www.youtube.com/embed/voorbeeld2"
+  }
+};
